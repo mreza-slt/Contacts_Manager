cbdd6c88bdc7ef439736179ca71cb6a48e82925a
m-reza <rsltany649@gmail.com>
2022-04-13 20:10:58 +0430

    Add project files.

---
+++ Contacts_Manager/Repository/IContactsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts_Manager
+{
+    internal interface IContactsRepository
+    {
+
+        DataTable SelectAll();
+        DataTable SelectRow(int contactId);
+
+        bool Inser(string name, string family, string mobile, string email, int age, string address);
+        bool Update(int contactId,string name, string family, string mobile, string email, int age, string address);
+        bool Delete(int contactId);
+    }
+}
---
+++ Contacts_Manager/Services/ContactsRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts_Manager
+{
+    internal class ContactsRepository : IContactsRepository
+    {
+        public bool Delete(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Inser(string name, string family, string mobile, string email, int age, string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectRow(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int contactId, string name, string family, string mobile, string email, int age, string address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
